e3cf950987e68db9c3b5d16118a5dde79f5cd29f
Yuqi Huai <yhuai@uci.edu>
2025-01-29 11:47:24 -0800

    save

--- anaconda.sh
+++
@@ -1,1 +0,0 @@
-wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
--- zoxide.sh
+++
@@ -1,6 +0,0 @@
-# check if zoxide is installed
-if ! command -v zoxide &> /dev/null
-then
-    # install zoxide
-    curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
-fi
