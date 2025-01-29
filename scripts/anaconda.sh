046189a2570f49bfbabc6c047cc12c4140a41099
Yuqi Huai <yhuai@uci.edu>
2025-01-29 12:12:21 -0800

    save

--- scripts/anaconda.sh
+++ scripts/anaconda.sh
@@ -1,1 +1,11 @@
+# exit if conda is already installed
+if command -v conda &> /dev/null
+then
+    echo "Anaconda is already installed"
+    exit
+fi
+
 wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
+bash Anaconda3-2024.10-1-Linux-x86_64.sh -b
+source ~/anaconda3/bin/activate
+conda init --all
