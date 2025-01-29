a04a42e923bbe6b969309826663a524e3b4fbc21
Yuqi Huai <yhuai@uci.edu>
2025-01-29 00:00:30 -0800

    initial commit

---
+++ astrovim.sh
@@ -0,0 +1,2 @@
+git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
+rm -rf ~/.config/nvim/.git
---
+++ zsh.sh
@@ -0,0 +1,10 @@
+apt install zsh # Install ZSH
+chsh -s $(which zsh) # Change default shell to ZSH
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
