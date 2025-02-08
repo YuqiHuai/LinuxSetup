1728d5fae02463044aae62e03ffd8e78d635b5ed
Yuqi Huai <yhuai@uci.edu>
2025-02-08 14:12:16 -0800

    impl: neovim install

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -6,6 +6,7 @@ then
 fi
 
 curl -LO https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.tar.gz
-rm -rf $HOME/.local/nvim
-tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-echo 'export PATH="$PATH:/opt/nvim-linux-x86_64//bin"' >> ~/.zshrc
+rm -rf $HOME/.local/nvim-linux-x86_64
+tar -C $HOME/.local -xzf nvim-linux-x86_64.tar.gz
+echo 'export PATH="$PATH:$HOME/.local/nvim-linux-x86_64/bin"' >> ~/.zshrc
+rm nvim-linux-x86_64.tar.gz
